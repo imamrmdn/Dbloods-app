9b4b1a6a3f8847e7c3c1c5218ac3714baa47b069
imamrmdn <imamramadhan420@gmail.com>
2020-09-03 21:29:36 +0700

    change profile screen

--- lib/ui/screens/profile_screen.dart
+++ lib/ui/screens/profile_screen.dart
@@ -9,50 +9,71 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     SizeTheme().init(context);
-    return Stack(
-      children: <Widget>[
-        BlocBuilder<UserBloc, UserState>(
-          builder: (_, userState) => (userState is UserLoaded)
-              ? ProfileIdentity(
-                  nama: '${userState.user.nama}',
-                  golDarah: 'Gol Darah ${userState.user.golDarah}',
-                  email: '${userState.user.email}',
-                  umur: (int.parse(formatAge(userState.user.tanggalLahir)) <=
-                          17)
-                      ? 'Belum Cukup Umur'
-                      : 'Umur ${formatAge(userState.user.tanggalLahir)} tahun',
-                )
-              : Loading(
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: Stack(
+        children: <Widget>[
+          //column
+          Column(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: Container(
                   color: mainColor,
-                  height: 50,
-                  width: 50,
-                ),
-        ),
-        Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            padding: EdgeInsets.all(15),
-            margin: EdgeInsets.only(top: SizeTheme.sizekVertical * 4),
-            height: SizeTheme.sizekVertical * 20, //200
-            width: SizeTheme.sizeHorizontal * 200, //200
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: whiteColor,
-            ),
-            child: Container(
-              height: SizeTheme.sizekVertical * 20, //200
-              width: SizeTheme.sizeHorizontal * 200,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: mainColor, width: 1),
-                image: DecorationImage(
-                  image: AssetImage('assets/user_profile.png'),
                 ),
               ),
-            ),
+              Flexible(
+                flex: 8,
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 150, 20, 70),
+                  child: ProfileMenu(),
+                ),
+              ),
+            ],
           ),
-        ),
-      ],
+          //heading profile identity
+          Card(
+            margin: EdgeInsets.only(top: 30, left: 20, right: 20),
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadius8,
+            ),
+            child: Container(
+              height: SizeConfig.defaultHeight / 5,
+              width: SizeConfig.defaultWidth,
+              padding: EdgeInsets.all(30),
+              child: BlocBuilder<UserBloc, UserState>(
+                builder: (_, userState) {
+                  if (userState is UserLoaded) {
+                    if (imageFileToUpload != null) {
+                      uploadImage(imageFileToUpload).then((downloadURL) {
+                        imageFileToUpload = null;
+                        context
+                            .bloc<UserBloc>()
+                            .add(UpdateData(profileImage: downloadURL));
+                      });
+                    }
+                    return ProfileCard(
+                      image: (userState.user.profilePicture == "")
+                          ? AssetImage('assets/user_profile.png')
+                          : NetworkImage(userState.user.profilePicture),
+                      nama: 'Nama: ${userState.user.nama}',
+                      golDarah: 'Gol Darah: ${userState.user.golDarah}',
+                      umur:
+                          'Umur: ${formatAge(userState.user.tanggalLahir)} tahun',
+                      email: '${userState.user.email}',
+                    );
+                  } else {
+                    return Center(
+                      child: Loading2(height: 40.0, width: 40.0),
+                    );
+                  }
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/ui/widgets/profilescreen_widget/profile_menu.dart
@@ -0,0 +1,96 @@
+part of '../widgets.dart';
+
+class ProfileMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        InkWell(
+          onTap: () {},
+          child: Row(
+            children: <Widget>[
+              SvgPicture.asset('assets/edit_profile.svg', height: 30),
+              SizedBox(width: 10),
+              Text('Edit Profile',
+                  style: blackTextFont.copyWith(fontWeight: FontWeight.bold))
+            ],
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Divider(color: greyColor),
+        SizedBox(height: 20.0),
+        //riwayat donor
+        InkWell(
+          onTap: () {},
+          child: Row(
+            children: <Widget>[
+              SvgPicture.asset('assets/stok.svg', height: 25),
+              SizedBox(width: 10),
+              Text('Riwayat Donor',
+                  style: blackTextFont.copyWith(fontWeight: FontWeight.bold))
+            ],
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Divider(color: greyColor),
+        SizedBox(height: 20.0),
+        //tentang dbloods
+        InkWell(
+          onTap: () {},
+          child: Row(
+            children: <Widget>[
+              SvgPicture.asset('assets/faq.svg', height: 30),
+              SizedBox(width: 10),
+              Text('Tentang Dbloods',
+                  style: blackTextFont.copyWith(fontWeight: FontWeight.bold))
+            ],
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Divider(color: greyColor),
+        SizedBox(height: 20.0),
+        //Rate Dbloods App
+        InkWell(
+          onTap: () {},
+          child: Row(
+            children: <Widget>[
+              SvgPicture.asset('assets/rate_dbloods.svg', height: 30),
+              SizedBox(width: 10),
+              Text('Rate Dbloods App',
+                  style: blackTextFont.copyWith(fontWeight: FontWeight.bold))
+            ],
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Divider(color: greyColor),
+        SizedBox(height: 20.0),
+        SignOutWidget(
+          onTap: () {
+            return showDialog(
+              context: context,
+              builder: (context) => ShowDialogSignOut(
+                onPressed1: () {
+                  Navigator.of(context).pop();
+                },
+                onPressed2: () {
+                  Navigator.of(context).pop();
+                  context.bloc<UserBloc>().add(SignOut());
+                  AuthServices.signOut();
+                  showFlushbarCustom(context,
+                      icon: Icon(Icons.done_outline),
+                      title: 'Anda telah sign out dari dbloods',
+                      text: 'silahkan sign in kembali',
+                      color: Colors.greenAccent);
+                },
+              ),
+            );
+          },
+        ),
+        SizedBox(height: 20.0),
+        Divider(color: greyColor),
+      ],
+    );
+  }
+}
