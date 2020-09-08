5dfa3017a577ed7212fb39dce0aae2fac9f100c9
imamrmdn <imamramadhan420@gmail.com>
2020-09-08 09:45:51 +0700

    update info screen

--- lib/ui/screens/info_screen.dart
+++ lib/ui/screens/info_screen.dart
@@ -1,6 +1,11 @@
 part of 'screens.dart';
 
-class InfoScreen extends StatelessWidget {
+class InfoScreen extends StatefulWidget {
+  @override
+  _InfoScreenState createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -29,36 +34,88 @@ class InfoScreen extends StatelessWidget {
                     Text('Edukasi', style: TextStyle(fontSize: 18)),
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ),
         body: TabBarView(
           children: <Widget>[
+            //Informasi
             Container(
               width: SizeConfig.defaultWidth,
               height: SizeConfig.defaultHeight,
               color: whiteColor,
               padding: EdgeInsets.only(bottom: 80.0),
-              child: ListView.builder(
-                  itemCount: informasi.length,
-                  itemBuilder: (context, index) {
-                    return CardCustomInformasi(
-                      informasi: informasi[index],
-                    );
-                  }),
+              child: RefreshIndicator(
+                color: mainColor,
+                onRefresh: refreshInformasi,
+                child: BlocBuilder<InformasiBloc, InformasiState>(
+                    builder: (_, informasiState) {
+                  if (informasiState is InformasiLoaded) {
+                    List<Informasi> informasi = informasiState.informasi;
+                    if (informasi == null ||
+                        informasi.isEmpty ||
+                        informasi == []) {
+                      return noDataYet('Informasi');
+                    }
+                    return ListView.builder(
+                        itemCount: informasi.length,
+                        itemBuilder: (context, i) {
+                          return CardCustomInformasi(
+                            informasi: informasi[i],
+                            onTap: () {},
+                          );
+                        });
+                  } else {
+                    return LoadingShimmerInfo();
+                  }
+                }),
+              ),
             ),
+            //Edukasi
             Container(
               width: SizeConfig.defaultWidth,
               height: SizeConfig.defaultHeight,
               color: whiteColor,
-              child: Center(
-                child: Text('tab Edukasi'),
+              padding: EdgeInsets.only(bottom: 80.0),
+              child: RefreshIndicator(
+                color: mainColor,
+                onRefresh: refreshEdukasi,
+                child: BlocBuilder<EdukasiBloc, EdukasiState>(
+                  builder: (_, edukasiState) {
+                    if (edukasiState is EdukasiLoaded) {
+                      List<Edukasi> edukasi = edukasiState.edukasi;
+                      if (edukasi == null || edukasi.isEmpty || edukasi == []) {
+                        return noDataYet('Edukasi');
+                      }
+                      return ListView.builder(
+                          itemCount: edukasi.length,
+                          itemBuilder: (context, i) {
+                            return EdukasiCard(
+                              edukasi: edukasi[i],
+                              onTap: () {},
+                            );
+                          });
+                    } else {
+                      return LoadingShimmerEdukasi();
+                    }
+                  },
+                ),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Future<Null> refreshInformasi() async {
+    context.bloc<InformasiBloc>().add(FetchInformasi());
+    await Future.delayed(Duration(seconds: 2));
+  }
+
+  Future<Null> refreshEdukasi() async {
+    context.bloc<EdukasiBloc>().add(FetchEdukasi());
+    await Future.delayed(Duration(seconds: 2));
   }
 }
